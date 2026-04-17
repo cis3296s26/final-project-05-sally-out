ba4023fa459c366222409ff87f3429214698f96e
BotWhan <graydenstraub@gmail.com>
2026-04-16 21:31:08 -0400

    delete bloat. modify ui to better suit our needs.

--- core/src/mindustry/ui/fragments/MenuFragment.java
+++ core/src/mindustry/ui/fragments/MenuFragment.java
@@ -188,18 +188,18 @@ public class MenuFragment{
             t.name = "buttons";
 
             buttons(t,
-                new MenuButton("@play", Icon.play,
+                new MenuButton("@playasd", Icon.play,
                     new MenuButton("@campaign", Icon.play, () -> checkPlay(ui.planet::show)),
                     new MenuButton("@joingame", Icon.add, () -> checkPlay(ui.join::show)),
                     new MenuButton("@customgame", Icon.terrain, () -> checkPlay(ui.custom::show)),
                     new MenuButton("@loadgame", Icon.download, () -> checkPlay(ui.load::show))
                 ),
-                new MenuButton("@database.button", Icon.menu,
-                    new MenuButton("@schematics", Icon.paste, ui.schematics::show),
-                    new MenuButton("@database", Icon.book, ui.database::show),
-                    new MenuButton("@about.button", Icon.info, ui.about::show)
-                ),
-                new MenuButton("@editor", Icon.terrain, () -> checkPlay(ui.maps::show)), steam ? new MenuButton("@workshop", Icon.steam, platform::openWorkshop) : null,
+                // new MenuButton("@database.button", Icon.menu,
+                //     new MenuButton("@schematics", Icon.paste, ui.schematics::show),
+                //     new MenuButton("@database", Icon.book, ui.database::show),
+                //     new MenuButton("@about.button", Icon.info, ui.about::show)
+                // ),
+                // new MenuButton("@editor", Icon.terrain, () -> checkPlay(ui.maps::show)), steam ? new MenuButton("@workshop", Icon.steam, platform::openWorkshop) : null,
                 new MenuButton("@mods", Icon.book, ui.mods::show),
                 new MenuButton("@settings", Icon.settings, ui.settings::show)
             );
